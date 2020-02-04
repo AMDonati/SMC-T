bdb75df89239359087ded83597a1a392bc5fc39c
AMDonati <alice.martindonati@gmail.com>
2020-02-04 08:36:00 +0100

    change in parser.args for easing the exp on various models for nlp

--- src/train/loss_functions.py
+++ src/train/loss_functions.py
@@ -7,19 +7,6 @@ import itertools
 
 from models.SMC_Transformer.transformer_utils import create_look_ahead_mask
 
-
-class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-  def __init__(self, d_model, warmup_steps=4000):
-    super(CustomSchedule, self).__init__()
-    self.d_model = d_model
-    self.d_model = tf.cast(self.d_model, tf.float32)
-    self.warmup_steps = warmup_steps
-  def __call__(self, step):
-    arg1 = tf.math.rsqrt(step)
-    arg2 = step * (self.warmup_steps ** -1.5)
-    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
-
-
 ### ----------------------- LOSS FUNCTIONS------------------------------------------------------------------------------
 
 def loss_function_classic_T_classif(real, pred, data_type):
