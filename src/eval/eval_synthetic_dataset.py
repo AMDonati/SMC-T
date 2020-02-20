eefb0f7f731ccc25f3a25e1c5fdcb22ab58bc866
AMDonati <alice.martindonati@gmail.com>
2020-02-20 09:22:45 +0100

    changes in training script for eval on synthetic dataset

--- src/eval/eval_synthetic_dataset.py
+++ src/eval/eval_synthetic_dataset.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 data_path = "../../data/synthetic_dataset.npy"
 target_path = '../../data/val_data_synthetic.npy'
-output_path = '/Users/alicemartin/000_Boulot_Polytechnique/07_PhD_thesis/code/SMC-T/output/azure_synthetic_dataset_exp/time_series_multi_synthetic_heads_1_depth_2_dff_8_pos-enc_None_pdrop_0.1_b_256_cs_True__particles_10_noise_True_sigma_0.05_smc-pos-enc_None/model_outputs/smc_predictions.npy'
+output_path = '/Users/alicemartin/000_Boulot_Polytechnique/07_PhD_thesis/code/SMC-T/output/azure_synthetic_dataset_exp/time_series_multi_synthetic_heads_1_depth_2_dff_8_pos-enc_None_pdrop_0.1_b_256_cs_True__particles_10_noise_True_sigma_0.05_smc-pos-enc_None/predictions_val_end_of_training.npy'
 
 predictions_val = np.load(output_path)
 
@@ -11,3 +11,19 @@ targets_val = np.load(target_path)
 
 print('predictions val shape', predictions_val.shape)
 print('targets_val shape', targets_val.shape)
+
+# reshaping predictions_val:
+shape_0 = predictions_val.shape[0]
+shape_1 = predictions_val.shape[1]
+shape_2 = predictions_val.shape[2]
+shape_3 = predictions_val.shape[3]
+predictions_val = np.reshape(predictions_val, newshape=(shape_0 * shape_1, shape_2, shape_3))
+print('predictions val - new shape', predictions_val.shape)
+num_samples_pred = predictions_val.shape[0]
+
+targets_val_trunc = targets_val[:num_samples_pred,:,:]
+targets_val_trunc = targets_val_trunc[:,1:,0]
+print('targets val truncated shape', targets_val_trunc.shape)
+
+predictions_val_reshape_path = output_path + '/predictions_val_end_training_reshaped.npy'
+
