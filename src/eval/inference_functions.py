correcting inference script with correct KL & wass distance

src/eval/inference_functions.py
@@ -2,6 +2,14 @@ import tensorflow as tf
 from models.SMC_Transformer.SMC_Transformer import SMC_Transformer
 from models.SMC_Transformer.transformer_utils import create_look_ahead_mask
 import numpy as np
+import scipy.stats
+
+import ot
+
+from utils.KL_divergences_estimators import naive_estimator
+
+#import scipy.stats.wasserstein_distance as wass_distance
+#import scipy.stats.entropy as KL_distance
 
 # def evaluate_one_timestep(model, num_samples, inputs, inp_seq_len):
 #   """
@@ -136,22 +144,6 @@ def inference_function_multistep(inputs, smc_transformer, N_prop, N_est, num_par
       tensor_pred_t = tf.squeeze(tf.squeeze(tensor_pred_t, axis=-1), axis=-1) # (B, N_est)
       list_preds_multistep.append(tensor_pred_t)
     tensor_preds_multistep = tf.stack(list_preds_multistep, axis=2)
-
-      #------------- OR... FOR SAMPLING PREDICTIONS...-----------------------------------------------------------------------------------------------
-      # for r in list_r_NP:
-      #   # reshape to have a tensor of shape (B,N,P,1,D)
-      #   new_shape = (tf.shape(r)[0], -1, N, num_particles, tf.shape(r)[-2], tf.shape(r)[-1])
-      #   r = tf.reshape(r, shape=new_shape)  # (B,-1,N,P,1,D)
-      #   r = tf.squeeze(r, axis=1)  # (B,N,P,1,D)
-      #   # select n* and p*:
-      #   p_ = tf.random.categorical(logits=w_s, num_samples=num_particles)
-      #   uniform_logits = tf.constant([[1 / N for _ in range(N)] for _ in range(tf.shape(r)[0])])
-      #   n_ = tf.random.categorical(logits=uniform_logits, num_samples=N)
-      #   r_ = tf.gather(r, p_, axis=2, batch_dims=1)
-      #   r_ = tf.gather(r_, n_, axis=1, batch_dims=1)  # (B,N,P,1,D)
-      #   mean_r_=smc_transformer.final_layer(r_) # (B,N,P,1,D)
-      #   X_pred = mean_r_ + tf.random.normal(shape=tf.shape(mean_r_), stddev=smc_transformer.omega) # (B,N,P,1,D)
-      #   list_preds_multistep.append(X_pred)
 
   return (list_r_NP, list_X_pred_NP), (list_preds_multistep, tensor_preds_multistep)
 
@@ -316,9 +308,6 @@ def generate_empirical_distribution(inputs, matrix_A, cov_matrix, N_est, num_tim
   return list_preds_sampled, tensor_preds_sampled
 
 
-
-
-
 if __name__ == "__main__":
   num_particles_training = 1
   seq_len = 24
@@ -414,9 +403,19 @@ if __name__ == "__main__":
   print('shape of tensor for multistep empirical distribution', tensor_preds_sampled.shape)
 
   # ----------- computation of the KL divergence ----------------------------------------------------------------------------------------
-  KL_measure = tf.keras.losses.KLDivergence()
+  #KL_measure = tf.keras.losses.KLDivergence()
+
   #KL_measure_2 = tf.keras.losses.KLDivergence(reduction=losses_utils.ReductionV2.NONE)
   for t, (true_distrib, pred_distrib) in enumerate(zip(list_empirical_dist, list_preds_sampled)):
-    KL_distance = KL_measure(y_true=true_distrib, y_pred=pred_distrib)
-    #KL_distance_2 = KL_measure_2(y_true=true_distrib, y_pred=pred_distrib)
+    #KL_distance = KL_measure(y_true=true_distrib, y_pred=pred_distrib)
+    true_distrib = tf.squeeze(true_distrib, axis=-1)
+    true_distrib = true_distrib.numpy()
+    pred_distrib = pred_distrib.numpy()
+    N_est = pred_distrib.shape[1]
+
+    #KL_dist = scipy.stats.entropy(pk=pred_distrib, qk=true_distrib, axis=1)
+    wass_dist = ot.emd2_1d(x_a=true_distrib[0,:], x_b=pred_distrib[0,:])
+    KL_dist = naive_estimator(true_distrib[0,:].reshape(N_est,1), pred_distrib[0,:].reshape(N_est,1))
+    #wass_dist = scipy.stats.wasserstein_distance(true_distrib, pred_distrib)
+
     print('KL distance for timestep {}: {}'.format(t, KL_distance))

src/eval/launch_inference_ts.py
@@ -10,6 +10,10 @@ from utils.utils_train import saving_inference_results
 from train.loss_functions import CustomSchedule
 from eval.inference_functions import inference_function_multistep_1D
 from eval.inference_functions import generate_empirical_distribution_1D
+import statistics
+
+import ot
+from utils.KL_divergences_estimators import naive_estimator
 
 import argparse
 import json
@@ -21,21 +25,21 @@ if __name__ == "__main__":
 
   parser = argparse.ArgumentParser()
   results_path = '/Users/alicemartin/000_Boulot_Polytechnique/07_PhD_thesis/code/SMC-T/output/post_UAI_exp/results_ws155_632020'
-  exp_path = 'time_series_multi_synthetic_heads_2_depth_6_dff_24_pos-enc_50_pdrop_0_b_256_target-feat_0_cv_False__particles_10_noise_True_sigma_0.05'
-  default_out_folder = os.path.join(results_path, exp_path)
+  exp_path = 'time_series_multi_synthetic_heads_2_depth_6_dff_24_pos-enc_50_pdrop_0_b_256_target-feat_0_cv_False__particles_1_noise_False_sigma_0.05'
   default_data_folder = '/Users/alicemartin/000_Boulot_Polytechnique/07_PhD_thesis/code/SMC-T/data/test_data_synthetic_3_feat.npy'
 
   #parser.add_argument("-config", type=str, help="path for the config file with hyperparameters")
-  parser.add_argument("-out_folder", default=default_out_folder, type=str, help="path for the output folder with training result")
+  parser.add_argument("-out_folder", default=exp_path, type=str, help="path for the output folder with training result")
   parser.add_argument("-data_path", default=default_data_folder, type=str, help="path for the test data folder")
-  parser.add_argument("-num_timesteps", default=4, type=int, help="number of timesteps for doing inference")
-  parser.add_argument("-p_inf", default=15, type=int, help="number of particles generated for inference")
+  parser.add_argument("-num_timesteps", default=1, type=int, help="number of timesteps for doing inference")
+  #parser.add_argument("-p_inf", default=15, type=int, help="number of particles generated for inference")
   parser.add_argument("-N", default=10, type=int, help="number of samples for MC sampling")
   #TODO: remove this one and do a for loop instead.
   #parser.add_argument("-N_est", type=int, help="number of samples for estimating the empirical distribution")
 
   args=parser.parse_args()
-  output_path = args.out_folder
+  exp_path = args.out_folder
+  output_path = os.path.join(results_path, exp_path)
   config_path = os.path.join(output_path, 'config.json')
   test_data_path = args.data_path
 
@@ -112,8 +116,10 @@ if __name__ == "__main__":
 
   # ---------------preparing the output path for inference -------------------------------------------------------------------------------------------------------------
   num_timesteps = args.num_timesteps
-  p_inf = args.p_inf
+  #p_inf = args.p_inf
   N = args.N
+  list_p_inf = [10,20,50]
+  N_est = 10000
 
   output_path = args.out_folder
   checkpoint_path = os.path.join(output_path, "checkpoints")
@@ -121,8 +127,8 @@ if __name__ == "__main__":
   if not os.path.isdir(os.path.join(output_path, 'inference_results')):
     output_path = create_run_dir(path_dir=output_path, path_name='inference_results')
   output_path = os.path.join(output_path, 'inference_results')
-  folder_template = 'num-timesteps_{}_p_inf_{}_N_{}'
-  out_folder=folder_template.format(num_timesteps, p_inf, N)
+  folder_template = 'num-timesteps_{}_p_inf_{}-{}-{}_N_{}_N-est_{}'
+  out_folder=folder_template.format(num_timesteps, list_p_inf[0], list_p_inf[1], list_p_inf[2], N, N_est)
   output_path = create_run_dir(path_dir=output_path, path_name=out_folder)
 
   # -------------- create the logging -----------------------------------------------------------------------------------------------------------------------------------
@@ -170,13 +176,13 @@ if __name__ == "__main__":
 
   # ------------------------------------- compute latest statistics as a check -----------------------------------------------------------------------------------------
   # ------------------------------------- compute inference timesteps --------------------------------------------------------------------------------------------------
-  list_num_samples = [5, 25, 50, 100, 250, 500, 1000]
+  #list_num_samples = [500, 1000, 5000, 10000, 50000]
   cov_matrix_3D = tf.constant([0.2, 0.3, 0.4], dtype=tf.float32)
   A_3D = tf.constant([[0.8, 0.1, 0], [0.2, 0.9, 0.2], [0, 0.1, 0.85]], dtype=tf.float32)
 
   list_KL_exp = []
-  for N_est in list_num_samples:
-    logger.info('inference results for number of samples: {}'.format(N_est))
+  for p_inf in list_p_inf:
+    logger.info('inference results for number of particles: {}'.format(p_inf))
 
     (list_r_NP, list_X_pred_NP), (list_preds_sampled, tensor_preds_sampled) = inference_function_multistep_1D(inputs=test_dataset,
                                                                                                        smc_transformer=smc_transformer,
@@ -192,26 +198,39 @@ if __name__ == "__main__":
                                                                                   cov_matrix=cov_matrix_3D,
                                                                                   N_est=N_est,
                                                                                   num_timesteps=num_timesteps)
-    KL_measure = tf.keras.losses.KLDivergence()
+    #KL_measure = tf.keras.losses.KLDivergence()
+    # KL_distance = KL_measure(y_true=true_distrib, y_pred=pred_distrib)
+    # KL_distance_norm = KL_distance / N_est
+    # KL_timesteps.append(KL_distance_norm.numpy())
+
     KL_timesteps = []
     for t, (true_distrib, pred_distrib) in enumerate(zip(list_empirical_dist, list_preds_sampled)):
-      KL_distance = KL_measure(y_true=true_distrib, y_pred=pred_distrib)
-      KL_distance_norm = KL_distance / N_est
-      KL_timesteps.append(KL_distance_norm.numpy())
-      logger.info('KL distance for timestep {}: {}'.format(t, KL_distance))
-      logger.info('KL distance normalized for timestep {}: {}'.format(t, KL_distance_norm))
-    list_KL_exp.append(KL_timesteps)
+      true_distrib = tf.squeeze(true_distrib, axis=-1)
+      true_distrib = true_distrib.numpy()
+      pred_distrib = pred_distrib.numpy()
+      batch_size = pred_distrib.shape[0]
+      num_samples = pred_distrib.shape[1]
+
+      #KL_dist = scipy.stats.entropy(pk=pred_distrib, qk=pred_distrib)
+      wassertein_dist_list = [ot.emd2_1d(x_a=true_distrib[i,:], x_b=pred_distrib[i,:]) for i in range(batch_size)]
+      wassertein_dist = statistics.mean(wassertein_dist_list)
+      KL_distance_list = [naive_estimator(true_distrib[i,:].reshape(num_samples,1), pred_distrib[i,:].reshape(num_samples,1)) for i in range(batch_size)]
+      KL_dist = statistics.mean(KL_distance_list)
+      logger.info('KL distance for timestep {}: {}'.format(t, KL_dist))
+      logger.info('wassertein distance for timestep {}: {}'.format(t, wassertein_dist))
+
+    #list_KL_exp.append(KL_timesteps)
     logger.info("<--------------------------------------------------------------------------------------------------------------------------------------------------------->")
 
   # ------------------------------------------------------- saving results on a csv file ---------------------------------------------------------------
 
-  keys = ['N_est = {}'.format(n) for n in list_num_samples]
-  keys = ['inference_timestep'] + keys
-  values = ['t+{}'.format(i+1) for i in range(num_timesteps)]
-  values = [values] + list_KL_exp
-  csv_fname='inference_results_KL_norm.csv'
-
-  saving_inference_results(keys=keys,
-                            values=values,
-                            output_path=output_path,
-                            csv_fname=csv_fname)
+  # keys = ['N_est = {}'.format(n) for n in list_num_samples]
+  # keys = ['inference_timestep'] + keys
+  # values = ['t+{}'.format(i+1) for i in range(num_timesteps)]
+  # values = [values] + list_KL_exp
+  # csv_fname='inference_results_KL_norm.csv'
+  #
+  # saving_inference_results(keys=keys,
+  #                           values=values,
+  #                           output_path=output_path,
+  #                           csv_fname=csv_fname)